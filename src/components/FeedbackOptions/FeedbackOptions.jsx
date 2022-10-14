95129ec1d1ec3d1f0983d10fd382c999324b413b
Anastasiia Nedbai <nedbayfomina@gmail.com>
2022-10-14 13:41:01 +0300

    FIX_OK

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './FeedbackOptions.css'
-import confetti from "https://cdn.skypack.dev/canvas-confetti@1";
+// import confetti from "https://cdn.skypack.dev/canvas-confetti@1";
 
 const FeedbackOptions = ({options, onLeaveFeedback}) => {
   const onClickBtn = (type) => () => {
-    confetti({
-      particleCount: 100,
-      spread: 150
-    });
+    // confetti({
+    //   particleCount: 100,
+    //   spread: 150
+    // });
     onLeaveFeedback(type);
   };
 
